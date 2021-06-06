dac896563145dc88108c4c807d2401044a1c2371
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2021-06-06 23:11:11 +0900

    [add] Hello Worldを追加

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -1,8 +1,16 @@
 package com.example
 
 import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.routing.*
 
 fun main(args: Array<String>) = io.ktor.server.netty.EngineMain.main(args)
 
 fun Application.main(){
+
+    routing {
+        get("/"){
+            call.respondText("Hello World!")
+        }
+    }
 }
