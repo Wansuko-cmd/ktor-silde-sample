7950377056d8c1f0235a8a30f66e84d818128fe3
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2021-06-06 23:09:42 +0900

    [add] application.confを追加

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -1,12 +1,8 @@
 package com.example
 
-import io.ktor.server.engine.*
-import io.ktor.server.netty.*
-import com.example.plugins.*
+import io.ktor.application.*
 
-fun main() {
-    embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
-        configureRouting()
-        configureTemplating()
-    }.start(wait = true)
+fun main(args: Array<String>) = io.ktor.server.netty.EngineMain.main(args)
+
+fun Application.main(){
 }
