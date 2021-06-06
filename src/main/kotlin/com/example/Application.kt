f20dd59175b290d42bccf7e9e706010115cfc294
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2021-06-06 23:27:59 +0900

    [add] queryを追加

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -1,6 +1,7 @@
 package com.example
 
 import io.ktor.application.*
+import io.ktor.http.*
 import io.ktor.response.*
 import io.ktor.routing.*
 
@@ -11,6 +12,18 @@ fun Application.main(){
     routing {
         get("/"){
             call.respondText("Hello World!")
+        }
+
+        route("/hello"){
+
+            get("{name}"){
+                val name = call.parameters["name"] ?: return@get call.respondText(
+                    "Bad Request",
+                    status = HttpStatusCode.BadRequest
+                )
+
+                call.respondText("Hello, $name")
+            }
         }
     }
 }
